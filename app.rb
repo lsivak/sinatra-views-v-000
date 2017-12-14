06d2f88c04a59a637e0e5719387b0d8be4bb433a
Lara Sanders <larasanders@gmail.com>
2017-12-14 18:07:05 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,9 +2,9 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-	# get '/' do
-  #   erb :index
-  #   end
+	get '/' do
+    erb :index
+    end
 
     get "/info" do
     "Testing the info page"
